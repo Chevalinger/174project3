30744a69fb8e88a89a7f36d080209f5b9ebf9551
Chevalinger <Ling@dhcp-10-4-9-122.wireless.rochester.edu>
2016-12-15 18:00:24 -0500

    Step1
    Homepage basically done. F and Z patterns. Long-scrolly.

---
+++ homepage.php
@@ -0,0 +1,102 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <title>CSC174 Project3</title>
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Libre+Baskerville" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<?php include 'inc/nav.inc'; ?>
+
+	 <div class="parallax-container valign-wrapper">
+      <div class="parallax"><img src="images/parallax1.png"></div>
+    </div>
+
+<div class="row">
+	<div class="col l5 s10" id="z1">
+		<h1>Personal Information</h1>
+		<p class="homep">My name is Ling Li. I am currently a student (Senior) from University of Rochester. I plan to go to graduate school after college. My dream program is something similar to <span class="#a5d6a7 green lighten-3">East Asian Studies.</span> </p>
+	</div>
+	<div class="col l5 s10" id="z2">
+		<h1>Other Things</h1>
+		<p class="homep">My hobby is listening to music, <span class="#a5d6a7 green lighten-3">teaching Chinese language and culture</span>, playing computer games and learning new languges. For sports, I can play badminton and skiing. Rochester is full of snow :)</p>
+	</div>
+</div><!--.row-->
+
+
+<div class="parallax-container valign-wrapper">
+   <div class="parallax"><img src="images/parallax2.png"></div>
+</div>
+
+
+<div class="row homesec2">
+<h2 class="homeh2">Major accomplishments</h2>
+
+<article class="col l8">
+
+<div class="sec2-1 col s12" id="f1">
+
+	<h3>Undergraduate Studies</h3>
+
+	<p>Bachelor of Arts in Business, Minor in Japanese, Cluster in Environmental Science by the end of December, 2016. Courses taken include: </p>
+
+	<ul>
+	    <li><span class="#a5d6a7 green lighten-3">CSC170: Web Design and Development</span></li>
+		<li><span class="#a5d6a7 green lighten-3">CSC174: Advanced Web Design and Development</span></li>
+		<li>STR241: Pricing Strategy</li>
+		<li>JPN101-202: Japanese</li>
+		<li>CHI214: Dream of the Red Chamber</li>
+		<li>and so on...</li>
+	</ul>
+
+</div><!--.sec2-1 col l6 s12-->
+
+<div class="sec2-1 col s12" id="f2">
+
+	<h3>Volunteer Teaching Experiences</h3>
+
+	<p>Volunteer experiences in teaching in Chinese elementary schools and in University of Rochester. Students ages from elementary school to English-speaking college students who are interested in Chinese language and culture. Experiences include:</p>
+
+	<ul>
+		<li><span class="#a5d6a7 green lighten-3">Volunteer of Language Exchange Program</span></li>
+		<li>Volunteer of Chinese Corner</li>
+		<li>Volunteer Teacher as the English Oral Club Leader</li>
+	</ul>
+
+</div><!--.sec2-1 col l6 s12-->
+
+</article>
+
+<aside class="sec2-2 col l4 s12" id="f3">
+
+	<h3>Supporting Skills</h3>
+
+
+</aside>
+</div><!--.homesec2-->
+
+
+
+
+<script>
+$( document ).ready(function(){})
+    $(".button-collapse").sideNav();
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
+<script src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/carousel.js"></script>
+<script type="text/javascript" src="js/mobile-nav.js"></script>
+<script type="text/javascript" src="js/parallax.js"></script>
+
+</body>
+
+</html>
